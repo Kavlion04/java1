2407f718bbbdf14143fe3719d9dfdde4becbd797
aqqullo <xaqqulloabdusamotov@gmail.com>
2024-12-29 18:22:49 +0500

    af

---
+++ app.js
@@ -0,0 +1,26 @@
+// 1 masala
+// function DigitSum(json) {
+//     return json.toString().split("").reduce((a, b) => a + +b, 0);
+// }
+// // birinchi string xolga otkazamiz keyin esa hammasini bolib olamiz va u array xoliga otadi, .reduce yordamida qoshib olamiz !
+
+// console.log("Birinchi son yig'iindisi: ", DigitSum(123)); // berilgan sonning boshida 0 qatnashmasligi kerak!
+// console.log("Ikkinchi son yig'iindisi: ", DigitSum(995));
+// console.log("Uchinchi son yig'iindisi: ", DigitSum(555));
+
+
+
+//  2 masala
+
+// function Fact(n) {
+//     if (n === 1) {
+//         return n;
+//         // n 1 ga teng bo'lsa 1 qaytarilad !
+//     }
+//     return n * Fact(n - 1);
+//     // n sonidan 1 marta ayirib boriladi shunda factorial topiladi!
+// }
+
+// console.log(Fact(3));
+// console.log(Fact(4));
+// console.log(Fact(5));
